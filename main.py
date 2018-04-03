8ed4e87e06e00fc459ddef1132573c53058329e1
Erwin Bohnert <ejbohnert@yahoo.com>
2018-04-03 18:58:23 -0500

    added forms to edit.html

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, redirect, render_template
+
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+
+
+@app.route('/')
+def index():
+    return render_template('edit.html')
+
+
+app.run()
