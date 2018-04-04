b487c7d8c426a1496dcccb411b428be30edc2267
Erwin Bohnert <ejbohnert@yahoo.com>
2018-04-04 10:21:27 -0500

    added some functionality to main

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from flask import Flask, request, redirect, render_template
-
+import cgi
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+@app.route('/user-signup', methods=['POST'])
+def user_signup():
+    username = request.form['username']
+    password = request.form['password']
+    verify_pass = request.form['verify-pass']
+    email = request.form['email']
 
+    return
+
+@app.route('/welcome')
+def success():
+    return render_template('welcome.html')
 
 @app.route('/')
 def index():
-    return render_template('edit.html')
+    error = request.args.get("error")
+    return render_template('edit.html', error = error)
 
 
 app.run()
