aa8bbdb4a865a61abaa75368c86f363a431c33b2
Erwin Bohnert <ejbohnert@yahoo.com>
2018-04-06 10:04:28 -0500

    added .format to username-not currently working

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def user_signup():
     verify_pass = request.form['verify-pass']
     email = request.form['email']
 
-    if (not username) or (username.strip() == ''):
+    if (not username) or (username.strip() == ' '):
         error = "Please enter a username"
         return redirect("/?error=" + cgi.escape(error, quote=True))
     else:
@@ -20,7 +20,7 @@ def user_signup():
             error = "Username needs to be between 3 and 20 characters"
             return redirect("/?error=" + cgi.escape(error, quote=True))
         else:
-            if (not password) or (password.strip()== ''):
+            if (not password) or (password.strip()== ' '):
                 error = "Please enter a password"
                 return redirect("/?error=" + cgi.escape(error, quote=True))
             else:
